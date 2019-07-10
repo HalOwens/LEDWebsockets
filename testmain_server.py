393154d048348fc59a4e81ce3815bccb63c69297
Hal Owens <hal.owens@exbsolutions.com>
2019-07-10 14:09:52 -0700

    Added unit test

--- testmain_server.py
+++ testmain_server.py
@@ -4,11 +4,23 @@ import main_server
 
 
 class TestValidateString(unittest.TestCase):
-    def test_valid_string_valid(self):
+    def test_validate_string_valid(self):
         asyncio.run(main_server.validate_string("[[1, 1, 1], [1, 1, 1], [1, 1, 1]]"))
         for vals in main_server.get_colors():
             self.assertEqual(vals, 1)
 
+    def test_validate_string_invalid(self):
+        asyncio.run(main_server.validate_string("this string should not work"))
+        vals = main_server.get_colors()
+        self.assertEqual(vals.__next__(), 255)
+        self.assertEqual(vals.__next__(), 0)
+        self.assertEqual(vals.__next__(), 0)
+        self.assertEqual(vals.__next__(), 255)
+        self.assertEqual(vals.__next__(), 0)
+        self.assertEqual(vals.__next__(), 0)
+        self.assertEqual(vals.__next__(), 255)
+        self.assertEqual(vals.__next__(), 0)
+        self.assertEqual(vals.__next__(), 0)
 
 if __name__ == '__main__':
     unittest.main()
