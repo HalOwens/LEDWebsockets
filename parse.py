4deaaa8ce8a07cc25ce6969cb82bfbee07b2ba57
Hal Owens <hal.owens@exbsolutions.com>
2019-08-13 10:31:13 -0700

    lots of stuff has been done have not been committing

--- parse.py
+++ parse.py
@@ -1,5 +1,5 @@
 import re
-
+import math
 
 class Requirement:
     """Requirement Variables"""
@@ -15,7 +15,7 @@ class Requirement:
     keywords = r"IF |< |> |= |THEN |-"
     """The auto generated code"""
     generatedCode = ""
-
+    test = ["","",""]
 
     def print_all(self):
         print(self.TEST_TYPE)
@@ -26,10 +26,69 @@ class Requirement:
         print(self.DESC)
         print(self.CODE)
 
+    def eval_conditions(self):
+        conditions = re.findall(r'IF.*', self.CODE)
+        length = len(conditions)
+        min = 0
+        max = 0
+        preCond = ""
+        minCond = ""
+        maxCond = ""
+        for i in range(length):
+            min = int(re.findall(r'(?<=\()\d*', conditions[i])[0])
+            max = int(re.findall(r'\d*(?=\))', conditions[i])[0])
+            cond = re.findall(r'[<>]', conditions[i])[0]
+            preCond = re.findall(r'[a-zA-Z]+(?=\(\d+-\d+\) [<>])', conditions[i])[0]
+            postCond = re.findall(r'(?<=[<>] )\d+', conditions[i])[0]
+            if cond == "<":
+                minCond = postCond
+                self.test[i] += "\tdef test_less_than_" + postCond +"(self):\n"
+                for x in range(min, max+1):
+                    self.test[i] += "\t\tself.uut." + preCond + str(x) + ".set(" + str(int(postCond)-1) + ")\n"
+            elif cond == ">":
+                maxCond = postCond
+                self.test[i] += "\tdef test_greater_than_" + postCond + "(self):\n"
+                for x in range(min, max+1):
+                    self.test[i] += "\t\tself.uut." + preCond + str(x) + ".set(" + str(int(postCond)+1) + ")\n"
+        self.test[length] += "\tdef test_between_" + minCond + "_" + maxCond + "(self):\n"
+        for i in range (min, max+1):
+            self.test[length] += "\t\tself.uut." + preCond + str(i) + ".set(" + str(math.trunc((min+max)/2)) + ")\n"
+
+    def run_and_assert(self):
+        assertions = re.findall(r'ASSERT.*', self.CODE)
+        for i in range(len(self.test)):
+            preAssert = re.findall(r'[A-Za-z\d_-]+(?= =)', assertions[0])[0]
+            postAssert = re.findall(r'(?<== ).*', assertions[0])[0]
+            self.test[i] += "\t\tasyncio.run(self.uut." + self.FUNCTION + "())\n"
+            #Any other possibilities for this?
+            value = re.findall(r'(?<=.set\()-*\d*', self.test[i])[0]
+            value = int(value)
+            if value > 255:
+                value = 255
+            if value < 0:
+                value = 0
+            value = str(value)
+            postAssert = re.sub(r'input\d*', value, postAssert)
+            postAssert = re.sub(r',', ', ', postAssert)
+            self.test[i] += "\t\tself.assertEqual(self.uut." + preAssert + ", " + postAssert + ")\n\n"
+
+
+    def generate_tests(self):
+        words = re.split("[ \n]", self.CODE)
+        print(words)
+        if words.count("IF") > 0:
+            self.eval_conditions()
+        if words.count("ASSERT") > 0:
+            self.run_and_assert()
+        for tests in self.test:
+            self.generatedCode += tests
+
+
     def split_keywords(self):
         return re.split(self.keywords, self.CODE)
 
     def build_import_list(self):
+        self.generatedCode += "import unittest\n"
         self.generatedCode += "import " + self.FILE.split(".")[0] + "\n"
         with open(self.FILE) as file_f:
             file = file_f.read()
@@ -38,10 +97,11 @@ class Requirement:
 
     def build_test_class(self):
         self.generatedCode += "\nclass Test(unittest.TestCase):\n\n"
+        self.generatedCode += "\tuut = main_desktop.Desktop()"
+        self.generatedCode += "\n\n"
 
     def build_main(self):
         self.generatedCode += "\nif __name__ == '__main__':\n"
-        ##How can I make sure this isn't always hardcoded?
         self.generatedCode += "\tTk()\n"
         self.generatedCode += "\tunittest.main()\n"
 
@@ -57,6 +117,7 @@ def remove_manual_test(reqs):
         if re.match(r'(TEST_TYPE:(?: )*MANUAL)', req) is None:
             autoTest.append(req)
     return autoTest
+
 
 def classify_reqs(reqs):
     reqObjects = []
@@ -74,14 +135,15 @@ def classify_reqs(reqs):
 
 
 if __name__ == '__main__':
-    reqObjects = 0
     with open("requirements.txt") as requirements_f:
         requirements = requirements_f.read()
         requirements = get_req_blocks(requirements)
         autoRequirements = remove_manual_test(requirements)
         reqObjects = classify_reqs(autoRequirements)
     #reqObjects[0].print_all()
+    #reqObjects[0].pre_compile()
     reqObjects[0].build_import_list()
     reqObjects[0].build_test_class()
+    reqObjects[0].generate_tests()
     reqObjects[0].build_main()
     print(reqObjects[0].generatedCode)
--- testmain_desktop.py
+++ testmain_desktop.py
@@ -1,59 +1,55 @@
 import unittest
-import asyncio
 import main_desktop
 from tkinter import *
+from tkinter import ttk, StringVar
+import asyncio
+import websockets
 
 
-class TestBuildString(unittest.TestCase):
+class Test(unittest.TestCase):
 
-    uut = main_desktop.Desktop()
+	uut = main_desktop.Desktop()
 
-    def test_between_0_255(self):
-        """Verifies that when all inputs are valid they are put into a string of the proper form """
-        self.uut.input1.set(1)
-        self.uut.input2.set(2)
-        self.uut.input3.set(3)
-        self.uut.input4.set(4)
-        self.uut.input5.set(5)
-        self.uut.input6.set(6)
-        self.uut.input7.set(7)
-        self.uut.input8.set(8)
-        self.uut.input9.set(9)
+	def test_less_than_0(self):
+		self.uut.input1.set(-1)
+		self.uut.input2.set(-1)
+		self.uut.input3.set(-1)
+		self.uut.input4.set(-1)
+		self.uut.input5.set(-1)
+		self.uut.input6.set(-1)
+		self.uut.input7.set(-1)
+		self.uut.input8.set(-1)
+		self.uut.input9.set(-1)
+		asyncio.run(self.uut.build_string())
+		self.assertEqual(self.uut.tx_string, "[[0, 0, 0], [0, 0, 0], [0, 0, 0]]")
 
-        asyncio.run(self.uut.build_string())
-        self.assertEqual(self.uut.tx_string, "[[1, 2, 3], [4, 5, 6], [7, 8, 9]]")
+	def test_greater_than_255(self):
+		self.uut.input1.set(256)
+		self.uut.input2.set(256)
+		self.uut.input3.set(256)
+		self.uut.input4.set(256)
+		self.uut.input5.set(256)
+		self.uut.input6.set(256)
+		self.uut.input7.set(256)
+		self.uut.input8.set(256)
+		self.uut.input9.set(256)
+		asyncio.run(self.uut.build_string())
+		self.assertEqual(self.uut.tx_string, "[[255, 255, 255], [255, 255, 255], [255, 255, 255]]")
 
-    def test_greater_than_255(self):
-        """Verifies that any inputs over 255 are mapped to the value 255"""
-        self.uut.input1.set(256)
-        self.uut.input2.set(256)
-        self.uut.input3.set(256)
-        self.uut.input4.set(256)
-        self.uut.input5.set(256)
-        self.uut.input6.set(256)
-        self.uut.input7.set(256)
-        self.uut.input8.set(256)
-        self.uut.input9.set(256)
-
-        asyncio.run(self.uut.build_string())
-        self.assertEqual(self.uut.tx_string, "[[255, 255, 255], [255, 255, 255], [255, 255, 255]]")
-
-    def test_less_than_0(self):
-        """Verifies that any values under 0 are mapped to the value 0"""
-        self.uut.input1.set(-1)
-        self.uut.input2.set(-1)
-        self.uut.input3.set(-1)
-        self.uut.input4.set(-1)
-        self.uut.input5.set(-1)
-        self.uut.input6.set(-1)
-        self.uut.input7.set(-1)
-        self.uut.input8.set(-1)
-        self.uut.input9.set(-1)
-
-        asyncio.run(self.uut.build_string())
-        self.assertEqual(self.uut.tx_string, "[[0, 0, 0], [0, 0, 0], [0, 0, 0]]")
+	def test_between_0_255(self):
+		self.uut.input1.set(5)
+		self.uut.input2.set(5)
+		self.uut.input3.set(5)
+		self.uut.input4.set(5)
+		self.uut.input5.set(5)
+		self.uut.input6.set(5)
+		self.uut.input7.set(5)
+		self.uut.input8.set(5)
+		self.uut.input9.set(5)
+		asyncio.run(self.uut.build_string())
+		self.assertEqual(self.uut.tx_string, "[[5, 5, 5], [5, 5, 5], [5, 5, 5]]")
 
 
 if __name__ == '__main__':
-    Tk()
-    unittest.main()
+	Tk()
+	unittest.main()
